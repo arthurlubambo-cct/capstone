361e4378e8a41314bfd04f3806e93237dbc49552
Arthur Lubambo <arthur.lubambo@inclusio.io>
2024-01-05 02:20:27 +0000

    :sparkles: add script to separate audio, and prototype for similarity calculation with small test case
    Signed-off-by: Arthur Lubambo <arthur.lubambo@inclusio.io>

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## Evaluating the Potential of Similarity Estimation Using Hyperloglog for Query by Humming Problem
 
 This repository contains the code for the capstone project research
+
+## How to Prepare Data
+
+-1 Install the Demucs Separator in 
+
---
+++ separate_tracks.sh
@@ -0,0 +1,11 @@
+# Directory containing audio files
+inputDir="./input"
+# Output directory for separated tracks
+outputDir="./output"
+
+# Loop through each audio file in the input directory
+for file in "$inputDir"/*.wav; do
+    # Extracting filename without extension
+    filename=$(basename "$file" .wav)
+    demucs "$file" -o "$outputDir"
+done
